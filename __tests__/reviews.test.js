393e11b04f715571fcb66db295310ba2ae9be63d
Eve Stockstill <evestockstillpdx@gmail.com>
2020-01-07 15:22:11 -0800

    working review create route

--- __tests__/reviews.test.js
+++ __tests__/reviews.test.js
@@ -29,6 +29,12 @@ describe('app routes', () => {
       dateOfBirth: new Date(),
       placeOfBirth: 'Santa Clara'
     });
+    studio = await Studio.create({
+      name: 'Star',
+      city: 'New York',
+      state: 'New York',
+      country: 'United States'
+    });
     film = await Film.create({
       title: 'Devils Rejects',
       studio: studio._id,
@@ -38,15 +44,38 @@ describe('app routes', () => {
         actor: actor._id
       }]
     });
-    review = await Review.create({
+    reviewer = await Reviewer.create({
       name: 'Captain Spalding',
       company: 'Firefly reviews'
     });
     review = await Review.create({
       rating: 5,
-      reviewer: reviewer.id,
+      reviewer: reviewer._id,
       review: 'It was a killer movie',
-      film: film.id
+      film: film._id
     });
+  });
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+  it('creates a review', () => {
+    return request(app)
+      .post('/api/v1/reviews')
+      .send({
+        rating: 5,
+        reviewer: reviewer._id,
+        review: 'It was a killer movie',
+        film: film._id
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          rating: 5,
+          reviewer: reviewer._id.toString(),
+          review: 'It was a killer movie',
+          film: film._id.toString(),
+          __v: 0
+        });
+      });
   });
 });
